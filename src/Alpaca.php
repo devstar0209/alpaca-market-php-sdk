f71080b8258ffd2d437a98b9622ef8c43c6f1403
gitdevstar <danielwarhn.online@gmail.com>
2023-02-17 17:13:07 -0500

    add crypto bars

--- src/Alpaca.php
+++ src/Alpaca.php
@@ -49,6 +49,7 @@ class Alpaca
      */
     private $paths = [
         "stocks_bars"     => "v2/stocks/{symbol}/bars",
+        "crypto_bars"     => "v1beta2/crypto/bars",
     ];
 
     /**
--- src/Api/Crypto.php
+++ src/Api/Crypto.php
@@ -17,4 +17,16 @@ class Crypto
         $this->alpaca = $alpaca;
     }
 
+    /**
+     * The Bars API returns aggregate historical data for the requested security over a specified time period.
+     * 
+     * @param JSON $params
+     * 
+     * @return array
+     */
+    public function historicalBars($params)
+    {
+        return $this->alpaca->request('crypto_bars', $params)->contents();
+    }
+
 }
